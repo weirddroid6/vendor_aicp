61e4f2ad2e0f1744576e69a825882da27d1cc889
mosimchah <mosimchah@gmail.com>
2026-07-26 02:17:10 -0400

    x
    Change-Id: Ib7146510e0f4fa76e47182c0e5c71a6af31e2f7f

---
+++ prebuilt/common/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+# a wrapper for curl which provides wget syntax, for compatibility
+include $(CLEAR_VARS)
+LOCAL_MODULE := wget
+LOCAL_SRC_FILES := bin/wget
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_CHECK_ELF_FILES := false
+include $(BUILD_PREBUILT)
